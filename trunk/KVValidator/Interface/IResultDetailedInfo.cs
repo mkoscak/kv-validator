17f4f5509051e37b3ece3c9ecdef89c07bfaef67
michal.koscak@gmail.com <michal.koscak@gmail.com@c23a8c29-1389-4edb-da47-4b8acfe9f552>
2014-06-11 18:34:52 +0000

    Pridane 2 nove rozhrania a uprava interfaceov - zaklad modelu.

---
+++ trunk/KVValidator/Interface/IResultDetailedInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KVValidator.Interface
+{
+    /// <summary>
+    /// Detail vysledku validacie, napr. info o cisle riadku, na ktorom je problem a pod.
+    /// </summary>
+    public interface IResultDetailedInfo
+    {
+        /// <summary>
+        /// Cislo riadku
+        /// </summary>
+        int? LineNumber { get; }
+
+        // TODO dalsie informacie?
+    }
+}

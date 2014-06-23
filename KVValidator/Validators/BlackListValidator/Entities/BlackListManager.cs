fb730783027005a9171ee58a4072071e8b0e4e24
michal.koscak <michal.koscak@gmail.com>
2014-06-23 19:27:12 +0000

    Pridany zabudnuty BlackListManager.

---
+++ KVValidator/Validators/BlackListValidator/Entities/BlackListManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KVValidator.Sql;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace KVValidator.Validators.BlackListValidator.Entities
+{
+    /// <summary>
+    /// Spravuje polozky v tabule T_BLACKLIST
+    /// </summary>
+    public class BlackListManager
+    {
+        /// <summary>
+        /// Import dat z XML 'ds_dphz.xml'
+        /// </summary>
+        /// <param name="path"></param>
+        public static int ImportDataFromXml(string path)
+        {
+            var entities = new List<BlackListEntity>();
+
+            // nacitanie entit z xml
+            LoadEntitiesFromXml(path, entities);
+
+            // zmazanie starych zaznamov
+            var db = DbProvider.Instance;
+            db.ExecuteNonQuery(string.Format("delete from {0}", BlackListEntity.TABLE_NAME));
+
+            // ulozenie novych
+            entities.ForEach(e => e.Save());
+
+            return entities.Count;
+        }
+
+        private static void LoadEntitiesFromXml(string path, List<BlackListEntity> entities)
+        {
+            //var doc = XDocument.Load(path);
+            var doc = new XmlDocument();
+            doc.Load(path);
+            var dsDph = doc["ZoznamPlatitelovDPHsDovodomNaZrusenieRegistracie"]["DS_DPHZ"];
+            foreach (XmlElement el in dsDph)
+            {
+                var ble = new BlackListEntity();
+                ble.IcDph = el["IC_DPH"].InnerText;
+                ble.Nazov = el["NAZOV"].InnerText.Replace('"', ' ');
+                ble.Obec = el["OBEC"].InnerText;
+                ble.Psc = el["PSC"].InnerText;
+                ble.Adresa = el["ADRESA"].InnerText;
+                ble.RokPorusenia = Convert.ToInt32(el["ROK_PORUSENIA"].InnerText);
+                ble.DatumZverejnenia = el["DAT_ZVEREJNENIA"].InnerText;
+
+                entities.Add(ble);
+            }
+        }
+    }
+}

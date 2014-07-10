d198500c72b59f4151b90bbfd79e2cdd582bff47
michal.koscak <michal.koscak@gmail.com>
2014-07-10 22:19:36 +0000

    Pridany export do excelu.

--- Avat/Components/Progress.designer.cs
+++ Avat/Components/Progress.designer.cs
@@ -108,7 +108,7 @@
             this.Controls.Add(this.progressBar);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(526, 148);
+            this.MinimumSize = new System.Drawing.Size(526, 170);
             this.Name = "Progress";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;

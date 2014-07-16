e1805166c55168d2bb538156834525843d6515f9
michal.koscak@gmail.com <michal.koscak@gmail.com@c23a8c29-1389-4edb-da47-4b8acfe9f552>
2014-07-16 00:10:54 +0000

    Designove upravy vol.3.

--- trunk/Avat/Components/OpsToolRenderer.cs
+++ trunk/Avat/Components/OpsToolRenderer.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Avat.Components
 {
     class OpsToolRenderer : ToolStripSystemRenderer
     {
+        Pen hoverPen = Pens.DarkGray;
+        Color hoverTextColor = Color.DarkGray;
+
+        protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+        {
+            //base.OnRenderToolStripBorder(e);
+        }
+
+        protected override void OnRenderButtonBackground(ToolStripItemRenderEventArgs e)
+        {
+            var p = Pens.Gray;
+            if (e.Item.Bounds.Contains(e.ToolStrip.PointToClient(Cursor.Position)))
+                p = hoverPen;
+            
+            e.Graphics.DrawRectangle(p, 0, 0, e.Item.Bounds.Width - 1, e.Item.Bounds.Height - 1);
+            //base.OnRenderButtonBackground(e);
+        }
+
+        protected override void OnRenderDropDownButtonBackground(ToolStripItemRenderEventArgs e)
+        {
+            var p = Pens.Gray;
+            if (e.Item.Bounds.Contains(e.ToolStrip.PointToClient(Cursor.Position)))
+                p = hoverPen;
+
+            e.Graphics.DrawRectangle(p, 0, 0, e.Item.Bounds.Width - 1, e.Item.Bounds.Height - 1);
+            //base.OnRenderDropDownButtonBackground(e);
+        }
+
+        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
+        {
+            if (e.Item.Bounds.Contains(e.ToolStrip.PointToClient(Cursor.Position)))
+                e.TextColor = hoverTextColor;
+
+            base.OnRenderItemText(e);
+        }
     }
 }

3b9d714aa84287b25b0d20ed8ce466046ed69051
michal.koscak <michal.koscak@gmail.com>
2014-09-22 23:17:18 +0000

    Pridany RoundedButton a zmeny UI - progres, toolbar buttony a vysledky validacie.

--- Avat/Components/OpsToolRenderer.cs
+++ Avat/Components/OpsToolRenderer.cs
@@ -24,61 +24,33 @@ namespace Avat.Components
         SolidBrush buttonHover = new SolidBrush(MyColors.ButtonHover);
         SolidBrush buttonInact = new SolidBrush(MyColors.ButtonInactive);
 
-        public static void DrawRoundedRectangleFill(Graphics g, Rectangle r, int d, Brush b)
-        {
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
-
-            gp.AddArc(r.X, r.Y, d, d, 180, 90);
-            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
-            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
-            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
-            gp.AddLine(r.X, r.Y + r.Height - d, r.X, r.Y + d / 2);
-            g.FillPath(b, gp);
-            gp.Dispose();
-        }
-
-        public static void DrawRoundedRectangle(Graphics g, Rectangle r, int d, Pen b)
-        {
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
-
-            gp.AddArc(r.X, r.Y, d, d, 180, 90);
-            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
-            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
-            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
-            gp.AddLine(r.X, r.Y + r.Height - d, r.X, r.Y + d / 2);
-            g.DrawPath(b, gp);
-            gp.Dispose();
-        }
-
-        int rund = 7;
+        int rund = 4;
         protected override void OnRenderButtonBackground(ToolStripItemRenderEventArgs e)
         {
             if (!e.Item.Enabled)
             {
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonInact);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonInact);
                 return;
             }
 
             if (e.Item.Bounds.Contains(e.ToolStrip.PointToClient(Cursor.Position)))
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle( 0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonHover);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonHover);
             else
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonBack);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonBack);
         }
 
         protected override void OnRenderDropDownButtonBackground(ToolStripItemRenderEventArgs e)
         {
             if (!e.Item.Enabled)
             {
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonInact);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonInact);
                 return;
             }
 
             if (e.Item.Bounds.Contains(e.ToolStrip.PointToClient(Cursor.Position)))
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonHover);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonHover);
             else
-                DrawRoundedRectangleFill(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonBack);
+                Common.DrawRoundedRectangle(e.Graphics, new Rectangle(0, 0, e.Item.Bounds.Width, e.Item.Bounds.Height), rund, buttonBack);
         }
 
         protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
